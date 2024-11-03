86b613e44af015437804aa9aa60df2eab5f84ce2
Joshua Cruz <jcruz1998@gmail.com>
2024-11-03 16:37:44 -0500

    Array Lists

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> food = new ArrayList<String>();
+        food.add("pizza");
+        food.add("hamburger");
+        food.add("hotdog");
+
+        food.set(0, "sushi");
+        food.remove(2);
+        food.clear();
+
+        //for(int i=0; i<food.size(); i++){
+            //System.out.println(food.get(i));
+
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+
+
+        ArrayList<String> bakeryList = new ArrayList();
+        bakeryList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
+
+        ArrayList<String> produceList = new ArrayList();
+        produceList.add("tomatoes");
+        produceList.add("zucchini");
+        produceList.add("peppers");
+
+        ArrayList<String> drinksList = new ArrayList();
+        drinksList.add("soda");
+        drinksList.add("coffee");
+
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinksList);
+        System.out.println(groceryList.get(0).get(0));
+
+        }
+    }
